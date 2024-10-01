0a55174b3533cd84d7932e378a1c429bcba43d36
FireAtWill26 <95119333+FireAtWill26@users.noreply.github.com>
2024-10-02 05:46:00 +0900

    Add files via upload

--- HW1.sql
+++ HW1.sql
@@ -1,58 +1,75 @@
+use AdventureWorks2019
+go
+
+--Problem 1
 select ProductID, [Name], Color, ListPrice 
 from Production.Product
 
+--Problem 2
 select ProductID, [Name], Color, ListPrice 
 from Production.Product
 where ListPrice != 0
 
+--Problem 3
 select ProductID, [Name], Color, ListPrice 
 from Production.Product
 where Color is Null
 
+--Problem 4
 select ProductID, [Name], Color, ListPrice 
 from Production.Product
 where Color is not Null
 
+--Problem 5
 select ProductID, [Name], Color, ListPrice 
 from Production.Product
 where Color is not Null and ListPrice > 0
 
+--Problem 6
 select ProductID, [Name] + ' ' + Color as [Name and Color], ListPrice 
 from Production.Product
 where Color is not Null
 
+--Problem 7
 select 'NAME: ' + [Name] + ' -- COLOR: ' + Color as Detail
 from Production.Product
 where [Name] like '%Crankarm%' or [Name] like 'Chainring%'
 order by ProductNumber
 
+--Problem 8
 select ProductID, [Name]
 from Production.Product
 where ProductID between 400 and 500
 
+--Problem 9
 select ProductID, [Name], Color
 from Production.Product
 where Color in ('Blue', 'Black')
 
+--Problem 10
 select ProductID, [Name], Color, ListPrice  from
 Production.Product
 where [Name] like 'S%'
 
+--Problem 11
 select [Name], ListPrice
 from Production.Product
 where [Name] like 'S%'
 order by [Name]
 
+--Problem 12
 select [Name], ListPrice
 from Production.Product
 where [Name] like 'S%' or [Name] like 'A%'
 order by [Name]
 
+--Problem 13
 select [Name], ListPrice
 from Production.Product
 where [Name] like 'SPO[^K]%'
 order by [Name]
 
+--Problem 14
 select distinct color
 from Production.Product
 order by color desc
